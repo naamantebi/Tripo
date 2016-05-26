4a9a989ba879df0f825bbbe7d6a4434d3fecfecb
naama <naamantebi@gmail.com>
2016-05-26 20:47:15 +0300

    login page

---
+++ app/src/main/java/com/example/hi/tripo/createProfile.java
@@ -0,0 +1,13 @@
+package com.example.hi.tripo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class createProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}
